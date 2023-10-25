2bd1c4756ab131ec90b67400b813e264480a3bca
Ariel <beroiza.ariel@gmail.com>
2023-10-25 01:06:50 -0300

    Mi primer commit

---
+++ public/script.js
@@ -0,0 +1,121 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const visitaId = document.getElementById('visitaId');
+  const nombre = document.getElementById('Nombre');
+  const zona = document.getElementById('Zona');
+  const hospedajesList = document.getElementById('hospedajesList');
+  const asignarVisitaBtn = document.getElementById('asignarVisitaBtn');
+
+
+  // Llena el select con los IDs de visitas al cargar la página.
+  fetch('/visitas')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((visita) => {
+        const option = document.createElement('option');
+        option.value = visita.IDVisita;
+        option.text = visita.IDVisita;
+        visitaId.appendChild(option);
+      });
+    });
+
+  // Maneja el evento de selección de un ID.
+  visitaId.addEventListener('change', () => {
+    const selectedId = visitaId.value;
+    if (selectedId) {
+      // Hace una solicitud al servidor para obtener los detalles de la visita seleccionada.
+      fetch(`/visitas/${selectedId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.length > 0) {
+            const visita = data[0];
+            nombre.textContent = visita.Nombre;
+            zona.textContent = visita.Zona;
+
+            // Una vez que tienes la "Zona" de la visita, obtén los hospedajes disponibles para esa "Zona".
+            fetch(`/hospedajes?zona=${visita.Zona}`)
+              .then((response) => response.json())
+              .then((hospedajes) => {
+                hospedajesList.innerHTML = ''; // Limpia la lista antes de agregar nuevos datos.
+
+              hospedajes.forEach((hospedaje) => {
+                const radioBtn = document.createElement('input');
+                radioBtn.type = 'radio';
+                radioBtn.name = 'hospedaje';
+                radioBtn.value = hospedaje.IDHospedaje;
+                const label = document.createElement('label');
+                label.textContent = `${hospedaje.Nombre} (Zona: ${hospedaje.Zona})`;
+                const br = document.createElement('br');
+
+                hospedajesList.appendChild(radioBtn);
+                hospedajesList.appendChild(label);
+                hospedajesList.appendChild(br);
+              });
+
+              })
+              .catch((error) => {
+                console.error(error);
+                hospedajesList.innerHTML = '<p>Error al obtener los datos de hospedajes.</p>';
+              });
+          } else {
+            nombre.textContent = '';
+            zona.textContent = '';
+            hospedajesList.innerHTML = '';
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          nombre.textContent = 'Error al obtener los datos';
+          zona.textContent = 'Error al obtener los datos';
+        });
+    } else {
+      nombre.textContent = '';
+      zona.textContent = '';
+      hospedajesList.innerHTML = '';
+    }
+  });
+
+// Botón "Asignar visita"
+asignarVisitaBtn.addEventListener('click', () => {
+  const selectedHospedajeId = document.querySelector('input[name="hospedaje"]:checked')?.value;
+  const selectedVisitaId = visitaId.value;
+
+  if (selectedHospedajeId && selectedVisitaId) {
+    fetch(`/asignar-visita/${selectedVisitaId}/${selectedHospedajeId}`, {
+      method: 'POST',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // Asignación exitosa, muestra un mensaje
+          mostrarMensaje("Asignación realizada");
+          // Actualiza la página para reflejar la asignación.
+          //location.reload();
+        } else {
+          console.error('Error al asignar la visita.');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        console.error('Error al realizar la solicitud de asignación de visita.');
+      });
+  }
+});
+});
+function mostrarMensaje(mensaje, exito) {
+  const mensajeDiv = document.getElementById('mensaje');
+  mensajeDiv.textContent = mensaje;
+
+  if (exito) {
+    mensajeDiv.classList.remove('error');
+    mensajeDiv.classList.add('exito');
+  } else {
+    mensajeDiv.classList.remove('exito');
+    mensajeDiv.classList.add('error');
+  }
+
+  mensajeDiv.style.display = 'block'; // Muestra el mensaje
+  setTimeout(() => {
+    mensajeDiv.style.display = 'none'; // Oculta el mensaje después de unos segundos
+  }, 3000); // Ajusta este valor según tus preferencias
+}
+
---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const { Client } = require('pg');
+const path = require('path');
+const bodyParser = require('body-parser'); // Agregamos body-parser para manejar datos POST
+
+const app = express();
+const port = 3000;
+
+const dbConfig = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'estudios',
+  password: 'ariel579',
+  port: 5432,
+};
+
+// Configura Express para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware para manejar datos POST
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/visitas', async (req, res) => {
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
+    const query = 'SELECT "IDVisita", "Nombre", "Zona" FROM visitas';
+    const { rows } = await client.query(query);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al consultar la base de datos' });
+  } finally {
+    await client.end();
+  }
+});
+
+app.get('/visitas/:id', async (req, res) => {
+  const id = req.params.id;
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
+    const query = 'SELECT "IDVisita", "Nombre", "Zona" FROM visitas WHERE "IDVisita" = $1';
+    const { rows } = await client.query(query, [id]);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al consultar la base de datos' });
+  } finally {
+    await client.end();
+  }
+});
+
+app.get('/hospedajes', async (req, res) => {
+  const zona = req.query.zona;
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
+    const query = 'SELECT "IDHospedaje", "Nombre", "Zona" FROM hospedajes WHERE "Zona" = $1';
+    const { rows } = await client.query(query, [zona]);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al consultar la base de datos' });
+  } finally {
+    await client.end();
+  }
+});
+
+// Nueva ruta para asignar un hospedador a una visita
+app.post('/asignar-visita/:visitaId/:hospedajeId', async (req, res) => {
+  const visitaId = req.params.visitaId;
+  const hospedajeId = req.params.hospedajeId;
+  const client = new Client(dbConfig);
+
+  try {
+    await client.connect();
+
+    // Primero, obtén el nombre del hospedaje seleccionado
+    const hospedajeQuery = 'SELECT "Nombre" FROM hospedajes WHERE "IDHospedaje" = $1';
+    const { rows: hospedajeRows } = await client.query(hospedajeQuery, [hospedajeId]);
+
+    if (hospedajeRows.length === 0) {
+      res.status(404).json({ success: false, message: 'Hospedaje no encontrado.' });
+      return;
+    }
+
+    const hospedajeNombre = hospedajeRows[0].Nombre;
+
+    // Luego, actualiza la visita con el hospedaje asignado
+    const asignarVisitaQuery = 'UPDATE visitas SET "Hospedador" = $1 WHERE "IDVisita" = $2';
+    await client.query(asignarVisitaQuery, [hospedajeNombre, visitaId]);
+
+    res.json({ success: true, message: 'Asignación realizada' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error al asignar la visita.' });
+  } finally {
+    await client.end();
+  }
+});
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
+});
